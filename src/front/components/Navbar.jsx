6aeed3634779e6cbc3e775acc98b3f65c0b81e87
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-29 03:42:31 +0000

    cambios

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -53,11 +53,45 @@ export const Navbar = () => {
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Sistema</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" aria-current="page" href="#">Beneficios</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" aria-current="page" href="#">Configuraciones</a>
+
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" role="button"data-bs-toggle="dropdown" aria-expanded="false">
+								Beneficios
+							</a>
+							<ul className="dropdown-menu fs-small">
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Pagina Web Personalizada</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Velocidad Garantizada</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Paginado de Tickets</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Migracion de Datos</a>
+								</li>
+							</ul>
+						</li>	
+
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" role="button"data-bs-toggle="dropdown" aria-expanded="false">
+								Configuraciones
+							</a>
+							<ul className="dropdown-menu fs-small">
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Dominio Web Propio</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Sistema de Pagos</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Sistema de Vendedores</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="scrollspyHeading1">Carga de Comprobante</a>
+								</li>
+							</ul>
 						</li>
 						<li className="nav-item">
 							<Link className="nav-link text-white bg-danger rounded-5 mx-2 px-3" to="/crear-rifa">Crear Rifa</Link>
